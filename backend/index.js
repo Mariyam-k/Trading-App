cdea5347d8a261358fc5f4daa5e237f237b21861
Mariyam khan <mariyamk@gmail.com>
2025-11-22 17:42:57 +0530

    commit changes

--- backend/index.js
+++ backend/index.js
@@ -11,15 +11,16 @@ const OrdersModel = require("./model/OrderModel");
 
 
 const PORT = process.env.PORT || 3002;
-// const url = "mongodb+srv://<dummy>:<dummy>@cluster.mongodb.net/db";
 const url = process.env.MONGO_URL;
 
 const app = express();
 
 app.use(express.json());
 // app.use(cors());
+
 app.use(cors({
   origin: ["http://localhost:5173", "http://localhost:5174"],
+   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
 app.use(bodyParser.json());
